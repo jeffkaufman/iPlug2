fd359f4c49970ef48bd5fe33470a5d99c53b7e3e
Jeff Kaufman <jeff@jefftk.com>
2020-01-25 20:03:32 -0500

    populate config

--- Examples/BassWhistle/config.h
+++ Examples/BassWhistle/config.h
@@ -3,10 +3,10 @@
 #define PLUG_VERSION_HEX 0x00010000
 #define PLUG_VERSION_STR "1.0.0"
 #define PLUG_UNIQUE_ID 'IsB4'
-#define PLUG_MFR_ID 'Acme'
-#define PLUG_URL_STR "https://iplug2.github.io"
-#define PLUG_EMAIL_STR "spam@me.com"
-#define PLUG_COPYRIGHT_STR "Copyright 2019 Acme Inc"
+#define PLUG_MFR_ID 'jefftk'
+#define PLUG_URL_STR "https://www.jefftk.com/bass-whistle"
+#define PLUG_EMAIL_STR "jeff@jefftk.com"
+#define PLUG_COPYRIGHT_STR "Copyright 2020 Jeff Kaufman"
 #define PLUG_CLASS_NAME BassWhistle
 
 #define BUNDLE_NAME "BassWhistle"
